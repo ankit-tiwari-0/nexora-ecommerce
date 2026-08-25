2ddf5591c720b6cff8638e20e54e47c6fd5f34af
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-25 20:34:27 +0530

    add hashing passw

--- nexora-api/models/user.model.js
+++ nexora-api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -36,5 +37,21 @@ const userSchema = new mongoose.Schema({
 },{
     timestamps: true
 })
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next(); 
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
 
  export const user = mongoose.model("user", userSchema)
