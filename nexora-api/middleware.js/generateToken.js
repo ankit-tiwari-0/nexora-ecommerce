5162c54a33bc4207f8705d43fc5d5e1c70fc2aba
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-27 12:24:47 +0530

    fix maxAge

--- nexora-api/middleware.js/generateToken.js
+++ nexora-api/middleware.js/generateToken.js
@@ -23,12 +23,12 @@ export const setCookies = (res, accessToken, refreshToken)=> {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        maxAge: 7 * 24 * 60 *1000,
+        maxAge:  15 * 60 * 1000
     })
     res.cookie("refToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        maxAge: 7 * 24 * 60 *1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 }
