e35eb12573ee4f693316fe41d163b3ef19968f8a
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-30 21:27:28 +0530

    add getFeaturedProduct api

--- nexora-api/controllers/product.controller.js
+++ nexora-api/controllers/product.controller.js
@@ -8,3 +8,27 @@ export const getAllproduct = async(req, res)=>{
         
     }
 }
+
+export const  getFeaturedProduct = async (req, res) => {
+    
+    try {
+        let featuredProducts = await redis.get("feature_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts))
+        }
+
+       featuredProducts = await PRODUCT.find({isFeatured:true}).lean() 
+
+       if (!featuredProducts) {
+        return res.status(400).json({message: "NO featured products found "})
+       }
+
+       await redis.set("feature_products",  JSON.stringify(featuredProducts));
+
+       res.json(featuredProducts)
+    } catch (error) {
+            console.log(error.message);
+            res.status(500).json({message:"server error", ero:error.message})
+
+    }
+}
--- nexora-api/routes/product.route.js
+++ nexora-api/routes/product.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllproduct } from "../controllers/product.controller.js";
+import { getAllproduct, getFeaturedProduct } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware.js/auth.middleware.js";
 
 const productRoute = Router()
 
 productRoute.get("/",protectRoute,adminRoute, getAllproduct)
+productRoute.get("/featured", getFeaturedProduct)
 
 
 export default productRoute
