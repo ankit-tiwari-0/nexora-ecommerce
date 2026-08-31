50032fc9cca85dfebde032791b3a930b3481b885
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-31 19:47:18 +0530

    add cartadd item

--- nexora-api/controllers/cart.container.js
+++ nexora-api/controllers/cart.container.js
@@ -1,5 +1,19 @@
 export const addcart = async(req,res)=>{
+   try {
+      const {productId} = req.body;
+      const user = req.user;
     
+      const existingItem = user.cartItem.find(item => item.id === productId);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else{
+        user.cartItem.push(productId)
+      }
+      await user.save();
+          res.json(user.cartItem)
+   } catch (error) {
+        res.status(500).json({message: error.message})
+   } 
 }
 export const getcartproduct = async(req,res)=>{
 
