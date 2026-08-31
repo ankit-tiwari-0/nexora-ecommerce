553dfa0987bfff0e116b8f461302d21dad4f5d95
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-31 19:53:05 +0530

    removeAllfromcart api use ti delete

--- nexora-api/controllers/cart.container.js
+++ nexora-api/controllers/cart.container.js
@@ -19,8 +19,29 @@ export const getcartproduct = async(req,res)=>{
 
 }
 export const removeAllFromcart = async(req,res)=>{
+   try {
+        const { productId } = req.body;
+        const user = req.user;
 
+        if (!productId) {
+            user.cartItems = [];
+        } else {
+            user.cartItems = user.cartItems.filter(
+                (item) => item.id !== productId
+            );
+        }
+
+        await user.save();
+
+        res.json(user.cartItems);
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
 }
 export const updatequantity = async(req,res)=>{
-
+   
 }
