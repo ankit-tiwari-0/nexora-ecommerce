3636186438704ab1308f8b58a74873ed804b9323
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-31 12:33:21 +0530

    add all route that is usr for cart

--- nexora-api/controllers/cart.container.js
+++ nexora-api/controllers/cart.container.js
@@ -1,3 +1,12 @@
 export const addcart = async(req,res)=>{
     
 }
+export const getcartproduct = async(req,res)=>{
+
+}
+export const removeAllFromcart = async(req,res)=>{
+
+}
+export const updatequantity = async(req,res)=>{
+
+}
--- nexora-api/routes/cart.route.js
+++ nexora-api/routes/cart.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { addcart } from "../controllers/cart.container.js";
+import { addcart, getcartproduct, removeAllFromcart, updatequantity } from "../controllers/cart.container.js";
+import { protectRoute } from "../middleware.js/auth.middleware.js";
 
 const cartRoute = Router()
 
-cartRoute.post("/add", addcart)
+cartRoute.post("/",protectRoute, addcart)
+cartRoute.delete("/",protectRoute, removeAllFromcart)
+cartRoute.put("/:id",protectRoute, updatequantity)
+cartRoute.get("/",protectRoute, getcartproduct)
 
 export default cartRoute
