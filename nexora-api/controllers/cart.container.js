c1e8d0980d3b39454d6b6a0d72759bfae4a7b191
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-31 19:54:43 +0530

    updatequantity

--- nexora-api/controllers/cart.container.js
+++ nexora-api/controllers/cart.container.js
@@ -1,25 +1,25 @@
-export const addcart = async(req,res)=>{
-   try {
-      const {productId} = req.body;
-      const user = req.user;
-    
-      const existingItem = user.cartItem.find(item => item.id === productId);
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else{
-        user.cartItem.push(productId)
-      }
-      await user.save();
-          res.json(user.cartItem)
-   } catch (error) {
-        res.status(500).json({message: error.message})
-   } 
+export const addcart = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const user = req.user;
+
+        const existingItem = user.cartItem.find(item => item.id === productId);
+        if (existingItem) {
+            existingItem.quantity += 1;
+        } else {
+            user.cartItem.push(productId)
+        }
+        await user.save();
+        res.json(user.cartItem)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
-export const getcartproduct = async(req,res)=>{
+export const getcartproduct = async (req, res) => {
 
 }
-export const removeAllFromcart = async(req,res)=>{
-   try {
+export const removeAllFromcart = async (req, res) => {
+    try {
         const { productId } = req.body;
         const user = req.user;
 
@@ -42,6 +42,42 @@ export const removeAllFromcart = async(req,res)=>{
         });
     }
 }
-export const updatequantity = async(req,res)=>{
-   
+export const updatequantity = async (req, res) => {
+    try {
+        const { id: productId } = req.params;
+        const { quantity } = req.body;
+        const user = req.user;
+
+        const existingItem = user.cartItems.find(
+            (item) => item.id === productId
+        );
+
+        if (existingItem) {
+            if (quantity === 0) {
+                user.cartItems = user.cartItems.filter(
+                    (item) => item.id !== productId
+                );
+
+                await user.save();
+                return res.json(user.cartItems);
+            }
+
+            existingItem.quantity = quantity;
+
+            await user.save();
+
+            return res.json(user.cartItems);
+
+        } else {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
 }
