b3e62b9803494007ac79d31cdb94e170532f35d4
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-09-06 13:15:11 +0530

    stripe setup

--- nexora-api/routes/payment.route.js
+++ nexora-api/routes/payment.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware.js/auth.middleware.js";
 
 const PAYMENT = Router()
 
-PAYMENT.post("/create-checkout-session")
+PAYMENT.post("/create-checkout-session", protectRoute, createCheckoutSession)
 
 export default PAYMENT
