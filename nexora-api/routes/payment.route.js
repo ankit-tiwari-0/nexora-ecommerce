c544444ce7b20c66430500a28e6531d4a1c79704
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-09-06 13:39:53 +0530

    createCheckoutSession payment controller

--- nexora-api/routes/payment.route.js
+++ nexora-api/routes/payment.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware.js/auth.middleware.js";
+import { createCheckoutSession } from "../controllers/payment.controller.js";
 
 const PAYMENT = Router()
 
