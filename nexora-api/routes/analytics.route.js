9070cf9becc9ad98280c258d15740d463ed76d57
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-09-08 13:00:13 +0530

    add getAnalyticData

--- nexora-api/routes/analytics.route.js
+++ nexora-api/routes/analytics.route.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
 import { adminRoute, protectRoute } from "../middleware.js/auth.middleware.js";
+import { getAnalyticData } from "../controllers/analutics.controller.js";
 
 const analytic_route = Router()
 
-get.analytic_route("/",protectRoute,adminRoute)
+get.analytic_route("/",protectRoute,adminRoute, async(req, res) =>{
+    try {
+        const analyticData = await getAnalyticData();
+
+        const endDate = new Date();
+        const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 1000);
+
+        const dailySaleDate = await getDailySaleDate(startDate, endDate);
+
+        res.json({
+            analyticData,
+            dailySaleDate,
+        });
+    } catch (error) {
+        console.log("error in analytic", error.message);
+            res.status(500).json({message: error.message})
+        
+    }
+})
  
+
 
 export default analytic_route
