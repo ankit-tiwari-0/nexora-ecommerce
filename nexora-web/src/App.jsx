db78c0373d23e5a440d3488064fc37a4ffef9ead
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-09-15 19:38:16 +0530

    login sign homepage add

--- nexora-web/src/App.jsx
+++ nexora-web/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/loginPage'
 
 const App = () => {
   return (
-    <div className='text-red-500'>hlooo</div>
+    <div>
+
+      <Routes>
+        <Route  path='/' element={<HomePage />} />
+        <Route  path='/signup' element={<SignupPage />} />
+        <Route  path='/login' element={<LoginPage/>} />
+      </Routes>
+    </div>
   )
 }
 
